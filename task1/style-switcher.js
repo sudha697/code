d782748912115706e60b724929bfaf931aedb58a
sudha697 <151456238+sudha697@users.noreply.github.com>
2024-06-16 16:31:31 +0530

    Add files via upload

---
+++ task1/style-switcher.js
@@ -0,0 +1,40 @@
+// Toggle style switcher
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggle.addEventListener("click", () => {
+    document.querySelector(".style-switcher").classList.toggle("open");
+});
+
+// Hide style switcher on scroll
+window.addEventListener("scroll", () => {
+    if (document.querySelector(".style-switcher").classList.contains("open")) {
+        document.querySelector(".style-switcher").classList.remove("open");
+    }
+});
+
+// Theme colors
+const alternateStyles = document.querySelectorAll(".alternate-style");
+function setActiveStyle(color) {
+    alternateStyles.forEach((style) => {
+        if (color === style.getAttribute("title")) {
+            style.removeAttribute("disabled");
+        } else {
+            style.setAttribute("disabled", "true");
+        }
+    });
+}
+//THEME LIGHT AND DARK MODE
+const dayNight=document.querySelector(".day-night");
+dayNight.addEventListener("click",()=>{
+    dayNight.querySelector("i").classList.toggle("fa-sun");
+    dayNight.querySelector("i").classList.toggle("fa-moon");
+    document.body.classList.toggle("dark");
+
+})
+window.addEventListener("load",()=>{
+    if(document.body.classList.contains("darK")){
+        dayNight.querySelector("i").classList.add("fa-sun");
+    }
+    else{
+        dayNight.querySelector("i").classList.add("fa-moon");
+    }
+})
